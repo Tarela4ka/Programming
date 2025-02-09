da95a73d0ad3eb02c70040a0a7e8705fd11c2592
Tarela4ka <mamlienkok@gmail.com>
2025-02-09 14:37:55 +0300

    modified:   Olimpiads/BelOI/2024-2025/II Stage/I/A.cpp 	modified:   Olimpiads/BelOI/2024-2025/II Stage/I/E.cpp 	new file:   Olimpiads/BelOI/2024-2025/III Stage/I/A.cpp 	new file:   Olimpiads/IOIP/A.cpp 	new file:   Olimpiads/IOIP/C.cpp 	modified:   Training/CodeForces/Div2/Codeforces Round 980/A.cpp 	new file:   Training/CodeForces/Div2/Codeforces Round 980/B.cpp 	new file:   Training/CodeForces/Div2/Codeforces Round 980/C.cpp

--- Olimpiads/IOIP/C.cpp
+++ Olimpiads/IOIP/C.cpp
@@ -20,23 +20,7 @@ using vc = v<char>;
 using vvc = v<vc>;
 
 const int MOD = 1e9+7;
-const int INF = 1e10;
-
-int sum(int a, int b){
-    return (a+b)%MOD;
-}
-int mult(int a, int b){
-    return (a*b)%MOD;
-}
-int bin_pow(int n, int p){
-    if (p == 0) return 1;
-    if (p == 1) return n;
-    if (p%2 == 1) return mult(bin_pow(mult(n, n), p/2), n);
-    return bin_pow(mult(n, n), p/2);
-}
-int inv(int n){
-    return bin_pow(n, MOD-2);
-}
+const int INF = 1e16;
 
 signed main(){
     ios::sync_with_stdio(false); 
@@ -48,21 +32,32 @@ signed main(){
     freopen("output.txt", "w", stdout);
     #endif
 
-    int n, k; cin >> n >> k;
-    vi a(n); fe(c, a) cin >> c;
-    vi bins(n, 0), ans(n, 0), fact(k+1, 1);
-    fr(i, 2, k) fact[i] = mult(fact[i-1], i);
-    fr(i, 0, k){
-        int j = i%n;
-        bins[j] = sum(bins[j], mult(fact[k], mult(inv(fact[i]), inv(fact[k-i])) ));
-    }
-    fr(i, 0, n-1){
-        fr(j, 0, n-1){
-            int x = (i+j)%n;
-            ans[x] = sum(ans[x], mult(bins[j], a[i]));
+    int t; cin >> t;
+    while(t--){
+        int n, k; cin >> n >> k;
+        vi a(n); fe(c, a) cin >> c;
+        sort(all(a));
+        int ans = 0;
+        fr(i, 0, n-k-1){
+            int minR = INF, ind = 0, c = 1;
+            fr(j, 1, n-1){
+                if (a[j]==a[j-1]) c++;
+                else {
+                    if ((a[j]-a[j-1])*c <= minR){
+                        minR = (a[j]-a[j-1])*c;
+                        ind = j-1;
+                    }
+                    c = 1;
+                }
+            }
+            ans += minR;
+            for(int j = ind; a[j]==a[ind] && j >= 0; j--){
+                a[j] = a[ind+1];
+            }
         }
+        // fe(c, a) cout << c << ' ';
+        cout << ans << "\n";
     }
-    fe(c, ans) cout << c << " ";
 
     #ifdef DEBUG
     t2=clock();
--- Training/CodeForces/Div2/Codeforces Round 980/B.cpp
+++ Training/CodeForces/Div2/Codeforces Round 980/B.cpp
@@ -22,22 +22,6 @@ using vvc = v<vc>;
 const int MOD = 1e9+7;
 const int INF = 1e10;
 
-int sum(int a, int b){
-    return (a+b)%MOD;
-}
-int mult(int a, int b){
-    return (a*b)%MOD;
-}
-int bin_pow(int n, int p){
-    if (p == 0) return 1;
-    if (p == 1) return n;
-    if (p%2 == 1) return mult(bin_pow(mult(n, n), p/2), n);
-    return bin_pow(mult(n, n), p/2);
-}
-int inv(int n){
-    return bin_pow(n, MOD-2);
-}
-
 signed main(){
     ios::sync_with_stdio(false); 
     cin.tie(NULL); cout.tie(NULL);
@@ -47,22 +31,21 @@ signed main(){
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     #endif
-
-    int n, k; cin >> n >> k;
-    vi a(n); fe(c, a) cin >> c;
-    vi bins(n, 0), ans(n, 0), fact(k+1, 1);
-    fr(i, 2, k) fact[i] = mult(fact[i-1], i);
-    fr(i, 0, k){
-        int j = i%n;
-        bins[j] = sum(bins[j], mult(fact[k], mult(inv(fact[i]), inv(fact[k-i])) ));
-    }
-    fr(i, 0, n-1){
-        fr(j, 0, n-1){
-            int x = (i+j)%n;
-            ans[x] = sum(ans[x], mult(bins[j], a[i]));
+    
+    int t; cin >> t;
+    while(t--){
+        int n, k; cin >> n >> k;
+        vi a(n); fe(c, a) cin >> c; sort(all(a));
+        int i = 0, res = 0, m = 0;
+        while(res < k){
+            res += (a[i]-m)*(n-i);
+            m = a[i];
+            if (res < k){
+                i++;
+            }
         }
+        cout << k + i << "\n";
     }
-    fe(c, ans) cout << c << " ";
 
     #ifdef DEBUG
     t2=clock();
